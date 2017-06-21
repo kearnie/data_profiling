663c1cea278f38a8c21960ed2717bed1cb59873f
klin27@its.jnj.com <KLin27@its.jnj.com>
2017-06-21 12:08:18 -0400

    jquery start

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	$('.button1').fadeTo(400,1);
+	$('.button2').fadeTo(400,0.5);
+	$('.button3').fadeTo(400,0.5);
+	$('.button4').fadeTo(400,0.5);
+	$('.button5').fadeTo(400,0.5);
+	$('.button6').fadeTo(400,0.5);
+});
