c99cf4e39b9be42e94d45b0b4524df6b6ae23858
klin27@its.jnj.com <KLin27@its.jnj.com>
2017-06-21 13:03:03 -0400

    testing click buttons

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
-	$('.button1').fadeTo(400,1);
-	$('.button2').fadeTo(400,0.5);
-	$('.button3').fadeTo(400,0.5);
-	$('.button4').fadeTo(400,0.5);
-	$('.button5').fadeTo(400,0.5);
-	$('.button6').fadeTo(400,0.5);
+	$('.button1').fadeTo(500,0.5);
+	$('.button2').fadeTo(500,0.5);
+	$('.button3').fadeTo(500,0.5);
+	$('.button4').fadeTo(500,0.5);
+	$('.button5').fadeTo(500,0.5);
+	$('.button6').fadeTo(500,0.5);
+
+	$('.button1').click(function () {
+		$(this).fadeTo(500,1);
+	});
+	$('.button2').click(function () {
+		$(this).fadeTo(500,1);
+	});
+	$('.button3').click(function () {
+		$(this).fadeTo(500,1);
+	});
+	$('.button4').click(function () {
+		$(this).fadeTo(500,1);
+	});
+	$('.button5').click(function () {
+		$(this).fadeTo(500,1);
+	});
+	$('.button6').click(function () {
+		$(this).fadeTo(500,1);
+	});
 });
