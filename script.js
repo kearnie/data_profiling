a3749c5c0e8f5d5193dc12a186dbcb1149ce087d
klin27@its.jnj.com <KLin27@its.jnj.com>
2017-06-22 09:37:14 -0400

    button3 is good

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
-	/*$('.button1').fadeIn(500);
-	$('.button2').fadeTo(500,0.5);
+	$('mainbuttons').hide();
+	$('.button1').hover(function () {
+		$(this).toggleClass('button1hover');
+	});
+	$('.button3').click(function () {
+		$(this).toggleClass('highlighted');
+		$(this).toggleClass('button3hover');
+	});
+	/*$('.button2').fadeTo(500,0.5);
 	$('.button3').fadeTo(500,0.5);
 	$('.button4').fadeTo(500,0.5);
 	$('.button5').fadeTo(500,0.5);
