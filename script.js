508c8f995973b138f6de5805e0e7c374963da6b3
klin27@its.jnj.com <KLin27@its.jnj.com>
2017-06-22 14:27:04 -0400

    sidebar buttons good

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
+// reset visuals of unclicked buttons; pass in string arg
+function resetbutton(unclickedbutton) { 
+	//var currClass = $(unclickedbutton).attr('class');
+	//var hoverClass = unclickedbutton.concat('hover');
+	$(unclickedbutton).removeClass('highlighted');
+	$(unclickedbutton).removeClass('buttonhover');
+}
+
 $(document).ready(function () {
 	$('mainbuttons').hide();
-	$('.button1').hover(function () {
-		$(this).toggleClass('button1hover');
+	$('.button1').click(function () {
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button2');
+		resetbutton('.button3');
+		resetbutton('.button4');
+		resetbutton('.button5');
+		resetbutton('.button6');
+		//$('.button2').removeClass('highlighted');
+		//$('.button2').removeClass('buttonhover');
+	});
+	$('.button2').click(function () {
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button1');
+		resetbutton('.button3');
+		resetbutton('.button4');
+		resetbutton('.button5');
+		resetbutton('.button6');
 	});
 	$('.button3').click(function () {
-		$(this).toggleClass('highlighted');
-		$(this).toggleClass('button3hover');
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button1');
+		resetbutton('.button2');
+		resetbutton('.button4');
+		resetbutton('.button5');
+		resetbutton('.button6');
 	});
+	$('.button4').click(function () {
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button1');
+		resetbutton('.button2');
+		resetbutton('.button3');
+		resetbutton('.button5');
+		resetbutton('.button6');
+	});
+	$('.button5').click(function () {
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button1');
+		resetbutton('.button2');
+		resetbutton('.button3');
+		resetbutton('.button4');
+		resetbutton('.button6');
+	});
+	$('.button6').click(function () {
+		$(this).addClass('highlighted');
+		$(this).addClass('buttonhover');
+		resetbutton('.button1');
+		resetbutton('.button2');
+		resetbutton('.button3');
+		resetbutton('.button4');
+		resetbutton('.button5');
+	});
+	
 	/*$('.button2').fadeTo(500,0.5);
 	$('.button3').fadeTo(500,0.5);
 	$('.button4').fadeTo(500,0.5);
